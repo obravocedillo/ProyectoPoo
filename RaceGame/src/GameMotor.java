281eece10504fb07625c8990b8ca8093bac5e6ff
oliver bravo cedillo <oliverbravo45@gmail.com>
2018-04-23 08:44:35 -0500

    1.2.0

--- RaceGame/src/GameMotor.java
+++ RaceGame/src/GameMotor.java
@@ -1,28 +1,63 @@
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 
 public class GameMotor extends JFrame  implements Runnable{
 	
 	static double FPS = 30.0;
+	PanelJuego pj;
 	Vehicule vehicule;
+	boolean gameActive;
+	boolean isPressed;
 	
 	public GameMotor(){
 		super("Race Game");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setPreferredSize(new Dimension(1000, 1000));
+		this.vehicule = new Vehicule();
+		this.gameActive = true;
+		this.pj = new PanelJuego(vehicule);
+		this.add(pj);
 		this.pack();
 		this.setVisible(true);
 		Thread thread = new Thread(this);
 		thread.start();
-		this.vehicule = new Vehicule();
+		this.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				isPressed = false;
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				System.out.println(vehicule.getVelocity());
+				isPressed = true;
+				
+			}
+		});
 	}
-
+		
+	
+	
 	@Override
 	public void run() {
 		try {
-			System.out.println(FPS);
-			Thread.sleep(50);
+			while(gameActive){
+				System.out.println(isPressed);
+				Thread.sleep(100);
+				
+			}
+			
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
@@ -31,6 +66,7 @@ public class GameMotor extends JFrame  implements Runnable{
 	
 	public static void main(String[] args){
 			GameMotor gm = new GameMotor();
+			
 	}
 	
 }
--- RaceGame/src/Vehicule.java
+++ RaceGame/src/Vehicule.java
@@ -1,15 +1,27 @@
 import java.awt.Graphics;
 
 public class Vehicule {
-	private double velocity;
+	private int velocity;
+	int turn;
 	int height;
 	int width;
 	
+	
 	public Vehicule(){
-		
+		this.velocity = 50;
+		this.turn = 0;
+		this.width = 100;
+		this.height = 100;
 	}
 	
-	public void pintarComponente(Graphics g){
-		
+	
+	
+	public void setVelocity(int velocity){
+		this.velocity = velocity;
 	}
+	
+	public int getVelocity(){
+		return velocity;
+	}
+	
 }
