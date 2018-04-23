7e0846e7fae8a034f88703302fbe31510d1e9292
oliver bravo cedillo <oliverbravo45@gmail.com>
2018-04-23 14:56:17 -0500

    1.6.0

---
+++ RaceGame/src/Listener.java
@@ -0,0 +1,39 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Listener implements KeyListener {
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		 int code = e.getKeyCode();
+		 
+	     if (code == KeyEvent.VK_DOWN) {
+	    	 System.out.println("abajo");
+	    	 
+	    } else if (code == KeyEvent.VK_UP) {
+	    	System.out.println("arriba");
+
+	     }else if (code == KeyEvent.VK_LEFT) {
+	    	 System.out.println("izquierda");
+	               
+	     }else if (code == KeyEvent.VK_RIGHT) {
+	    	 System.out.println("derecha");
+
+	       }
+	         
+	    }
+	
+	
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
